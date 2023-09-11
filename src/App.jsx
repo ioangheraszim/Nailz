73108681de1776c4f5fb8812425bebdf929cb288
ioangheraszim <gijohny17@gmail.com>
2023-09-11 22:59:36 +0100

    made tailwind css work

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,9 @@ import './App.css'
 function App() {
 
   return (
-    <>
-    <div className='card'>
-      <a className='link' href="#">
-          <div>
-            <h1>Introducing our exquisite Rose Gold Manicure Set, the epitome of elegance and functionality. Crafted with precision and style in mind, this luxurious set includes all the essential tools for a flawless manicure. Each tool features a stunning rose gold finish, combining aesthetics with durability. Elevate your nail care routine and achieve salon-quality results from the comfort of your home with this opulent manicure set. Whether you're a professional nail technician or a self-care enthusiast, the Rose Gold Manicure Set is your passport to perfectly manicured nails.</h1>
-            <p>good bye world</p>
-          </div>
-      </a>
-    </div>
-    
-    </>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
 
