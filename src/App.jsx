2e41ae761881739728322a566afd06df9c35241e
ioangheraszim <gijohny17@gmail.com>
2023-09-15 12:09:45 +0100

    removed tailwind from project will use SASS instead

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Navbar from "./assets/components/Navbar";
+import "./App.scss";
 
 export default function App() {
   return (
@@ -6,21 +7,21 @@ export default function App() {
       <Navbar />
       <main>
         {/* Hero Section */}
-        <section className="bg-cardcolor wrapper">
-          <div className="mycontainer hero-container">
-            <div className="absolute info-card">
-              <h1 className="font-montserrat pl-[56px] text-[48px] font-[700]">
+        <section className="">
+          <div className="">
+            <div className="">
+              <h1 className="">
                 Online Classes
               </h1>
-              <p className="text-[14px] pl-[56px] text-left w-[450px] mb-5">
+              <p className="">
                 Join our education and learn the path in becoming a nail
                 technician and much more at low cost.
               </p>
-              <button className="btn btn-hero">Join Us</button>
+              <button className="">Join Us</button>
             </div>
-            <div className="bg-cardcolor">
+            <div className="">
               <img
-                className="animate-fade-in-right bg-cardcolor min-w-full ml-32"
+                className=""
                 src="./src/assets/images/nail-image-01.svg"
                 alt="hero image"
               />
@@ -28,99 +29,48 @@ export default function App() {
           </div>
         </section>
         {/* Most Popular Products */}
-        <section className="mycontainer my-10">
-          <h2 className="tablet:text-[40px] mobile:text-[25px] text-[36px] font-bold">
+        <section className="">
+          <h2 className="">
             Most <span className="text-primary">Popular</span> Products
           </h2>
           {/* Card Container */}
-          <div className="my-10 desktop:grid-cols-4 tablet:grid-cols-2 mobile:grid-cols-1 gap-6 transition grid ">
+          <div className="">
             {/* Card Element */}
-            <div className="p-3 rounded-lg shadow-2xl">
+            <div className="">
               <img
-                className="rounded-lg"
+                className=""
                 src="./src/assets/images/image-product.svg"
                 alt="product image"
               />
-              <div className="flex pt-4">
+              <div className="">
                 <img src="./src/assets/images/star.svg" alt="star image" />
-                <p className="ml-2 text-bold">(4.5)</p>
+                <p className="">(4.5)</p>
               </div>
-              <p className="py-2">Rose Gold Manicure Set</p>
-              <div className="flex justify-between items-center py-3">
+              <p className="">Rose Gold Manicure Set</p>
+              <div className="">
                 <p className="">$39.99</p>
-                <button className="btn btn-add">Add to Cart</button>
-              </div>
-            </div>
-            {/* Card Element */}
-            <div className="p-3 rounded-lg shadow-2xl">
-              <img
-                className="rounded-lg"
-                src="./src/assets/images/image-product.svg"
-                alt="product image"
-              />
-              <div className="flex pt-4">
-                <img src="./src/assets/images/star.svg" alt="star image" />
-                <p className="ml-2 text-bold">(4.5)</p>
-              </div>
-              <p className="py-2">Rose Gold Manicure Set</p>
-              <div className="flex justify-between items-center py-3">
-                <p className="">$39.99</p>
-                <button className="btn btn-add">Add to Cart</button>
-              </div>
-            </div>
-            {/* Card Element */}
-            <div className="p-3 rounded-lg shadow-2xl">
-              <img
-                className="rounded-lg"
-                src="./src/assets/images/image-product.svg"
-                alt="product image"
-              />
-              <div className="flex pt-4">
-                <img src="./src/assets/images/star.svg" alt="star image" />
-                <p className="ml-2 text-bold">(4.5)</p>
-              </div>
-              <p className="py-2">Rose Gold Manicure Set</p>
-              <div className="flex justify-between items-center py-3">
-                <p className="">$39.99</p>
-                <button className="btn btn-add">Add to Cart</button>
-              </div>
-            </div>
-            {/* Card Element */}
-            <div className="p-3 rounded-lg shadow-2xl">
-              <img
-                className="rounded-lg"
-                src="./src/assets/images/image-product.svg"
-                alt="product image"
-              />
-              <div className="flex pt-4">
-                <img src="./src/assets/images/star.svg" alt="star image" />
-                <p className="ml-2 text-bold">(4.5)</p>
-              </div>
-              <p className="py-2">Rose Gold Manicure Set</p>
-              <div className="flex justify-between items-center py-3">
-                <p className="">$39.99</p>
-                <button className="btn btn-add">Add to Cart</button>
+                <button className="">Add to Cart</button>
               </div>
             </div>
           </div>
         </section>
 
         {/* CTA Online Classes */}
-        <section className="mycontainer my-16 flex items-center border">
-          <div className="w-[50%] p-5">
-            <h2 className="text-[40px] font-bold mb-5">
-              <span className="text-primary">Online</span> Classes
+        <section className="">
+          <div className="">
+            <h2 className="">
+              <span className="">Online</span> Classes
             </h2>
-            <p className="my-10 max-w-[385px]">
+            <p className="">
               Elevate your nail game with our online nail technician classes.
               From the basics to advanced techniques, we'll guide you every step
               of the way. Join us and turn your passion into a profession!
             </p>
-            <button className="btn py-2 px-5">Join Now</button>
+            <button className="">Join Now</button>
           </div>
-          <div className="w-[50%]">
+          <div className="">
             <img
-              className="rounded-lg"
+              className=""
               src="./src/assets/images/online-classes.svg"
               alt="Online Classes Image"
             />
--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -2,49 +2,49 @@ import React from "react";
 
 function Navbar() {
   return (
-    <header className="mycontainer">
+    <header className="container">
       <input type="text" className="hidden" />
-      <nav className="flex justify-between items-center">
-        <ul className="flex items-center">
-          <div>
+      <nav className="">
+        <ul className="">
+          <div className="">
             <img
               src="./src/assets/images/logo.svg"
               alt="logo image"
               className=""
             />
           </div>
-          <div className="hidden tablet:flex">
-            <li className="p-3 hover:text-accent transition">
+          <div className="">
+            <li className="">
               <a href="#">Home</a>
             </li>
-            <li className="p-3 hover:text-accent transition">
+            <li className="">
               <a href="#">Products</a>
             </li>
-            <li className="p-3 hover:text-accent transition">
+            <li className="">
               <a href="#">Courses</a>
             </li>
-            <li className="p-3 hover:text-accent transition">
+            <li className="">
               <a href="#">About Us</a>
             </li>
           </div>
         </ul>
-        <ul className="flex items-center">
-          <li className="p-3 hover:text-accent transition">
+        <ul className="r">
+          <li className="">
             <button href="#">
               <img src="./src/assets/images/user-circle.svg" alt="" />
             </button>
           </li>
-          <li className="p-3 hover:text-accent transition">
+          <li className="">
             <button href="#">
               <img src="./src/assets/images/search.svg" alt="" />
             </button>
           </li>
-          <li className="p-3 hover:text-accent transition">
+          <li className="">
             <button href="#">
               <img src="./src/assets/images/shopping-bag.svg" alt="" />
             </button>
           </li>
-          <li className="p-3 hover:text-accent transition tablet:hidden">
+          <li className="">
             <button href="#">
               <img src="./src/assets/images/menu.svg" alt="" />
             </button>
