4b30ffbd8c83108f22a8a618d362c6004394d691
ioangheraszim <gijohny17@gmail.com>
2023-09-25 11:55:07 +0100

    finished desktop cart and checkout

--- src/assets/components/Sections/Cart/CartPage.jsx
+++ src/assets/components/Sections/Cart/CartPage.jsx
@@ -35,46 +35,54 @@ function Cart() {
               </div>
             </div>
           </div>
+        </div>
 
-          <div className="checkout-container">
-            <div className="co-title">
-              <h2>Check out</h2>
-              <div className="co-form">
-                <div className="info-labels">
-                  <div>
-                    <label className="" htmlFor="">First Name</label>
-                    <input className="" type="text" />
-                  </div>
-                  <div>
-                    <label htmlFor="">Last Name</label>
-                    <input type="text" />
-                  </div>
+        <div className="total-card">
+          <p className="total-p">Total:</p>
+          <p className="price-p">$39.99</p>
+        </div>
+
+        <div className="checkout-container">
+          <div className="co-title">
+            <h2>Check out</h2>
+            <div className="co-form">
+              <div className="info-labels">
+                <div>
+                  <label className="" htmlFor="">First Name</label>
+                  <input className="" type="text" />
                 </div>
-                <div className="email-input">
-                  <label htmlFor="email">Email</label>
-                  <input type="text" name="email" id="" />
+                <div>
+                  <label htmlFor="">Last Name</label>
+                  <input type="text" />
                 </div>
               </div>
+              <div className="email-input">
+                <label htmlFor="email">Email</label>
+                <input type="text" name="email" id="" />
+              </div>
             </div>
+          </div>
+          <div className="payment-card">
+            <div>
             <h2>Choose Payment Method</h2>
-            <div className="payment-card">
-              <div className="co-payment">
-                <div className="card-pt">
-                  <img className="payment-img" src="https://1000logos.net/wp-content/uploads/2017/06/VISA-Logo-1976.png" alt="" />
-                </div>
-                <div className="card-pt">
-                  <img className="payment-img" src="https://1000logos.net/wp-content/uploads/2021/04/Paypal-logo.png" alt="" />
-                </div>
-                <div className="card-pt">
-                  <img className="payment-img" src="https://download.logo.wine/logo/Apple_Pay/Apple_Pay-Logo.wine.png" alt="" />
-                </div>
-                <div className="card-pt">
-                  <img className="payment-img" src="https://lh3.googleusercontent.com/frPbBfyjNNNGAGT93LKGESTJXjVW8Y3aI5-rTW4uiIHldMgCsEOlx14GZ20mzK5nOznS-H2fWNyv-UpaEOTuDMOc-frsyTXlu2W4" alt="" />
-                </div>
+            </div>
+            <div className="co-payment">
+              <div className="card-pt">
+                <img className="payment-img" src="https://1000logos.net/wp-content/uploads/2017/06/VISA-Logo-1976.png" alt="" />
+              </div>
+              <div className="card-pt">
+                <img className="payment-img" src="https://1000logos.net/wp-content/uploads/2021/04/Paypal-logo.png" alt="" />
+              </div>
+              <div className="card-pt">
+                <img className="payment-img" src="https://download.logo.wine/logo/Apple_Pay/Apple_Pay-Logo.wine.png" alt="" />
+              </div>
+              <div className="card-pt">
+                <img className="payment-img" src="https://lh3.googleusercontent.com/frPbBfyjNNNGAGT93LKGESTJXjVW8Y3aI5-rTW4uiIHldMgCsEOlx14GZ20mzK5nOznS-H2fWNyv-UpaEOTuDMOc-frsyTXlu2W4" alt="" />
               </div>
             </div>
           </div>
         </div>
+
       </section>
       <Footer />
     </>
